8fb82d6e3f789759da4be3c1adc6ae2c199b7879
piotrkulkowski <58171765+piotrkulkowski@users.noreply.github.com>
2020-03-09 12:13:45 +0100

    Add files via upload

---
+++ angulardb/src/test/java/pl/piokul/angulardb/AngulardbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piokul.angulardb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngulardbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
