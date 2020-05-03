48c55c9f325529fc9461f86fcb140894c610439e
piotrkulkowski <58171765+piotrkulkowski@users.noreply.github.com>
2020-05-03 23:46:03 +0200

    Add files via upload

--- angulardbfront/src/app/app-routing.module.ts
+++ angulardbfront/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TabelaComponent } from './tabela/tabela/tabela.component';
+import { LogowanieComponent } from './logowanie/logowanie.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tabela', component: TabelaComponent},
+  { path: 'logowanie', component: LogowanieComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
